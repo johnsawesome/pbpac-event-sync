242094e0298347d919a5a846510801260b867a9d
johnsawesome <jmclark995@gmail.com>
2025-03-11 11:56:06 -0400

    Update main.py - more DST troubleshooting

--- main.py
+++ main.py
@@ -130,9 +130,8 @@ async def get_agenda_summary(agenda_text: str):
 async def build_event(event: dict) -> Event:
     _id = str(event['id'])
     name = event['eventName']
-    tz = pytz.timezone("America/New_York")
-    start_time = datetime.strptime(event['startDateTime'], '%Y-%m-%dT%H:%M:%SZ')  
-    start_time = pytz.utc.localize(start_time).astimezone(tz)
+    start_time = datetime.strptime(event['startDateTime'], '%Y-%m-%dT%H:%M:%SZ')
+    start_time = start_time.replace(tzinfo=pytz.timezone("America/New_York"))  # Just attach ET timezone
     end_time = start_time + timedelta(hours=2)
     logger.info(f"processing: '{name}' {start_time} - {end_time}")
     existing_meeting = meetings.get((name, start_time.isoformat()))
